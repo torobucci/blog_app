1281509999f338bdd631d3fdb50f823aa09014a4
torobucci <kevintoro254@gmail.com>
2023-06-15 13:22:57 +0300

    create models and add custom methods

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class Post < ApplicationRecord
+  has_many :comments
+  has_many :likes
+  belongs_to :user, foreign_key: 'author_id', counter_cache: :posts_counter
+
+  def update_user_posts_counter
+    if saved_change_to_author_id? || destroyed?
+      User.reset_counters(user.id, :posts)
+    end
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+end
