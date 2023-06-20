13f717b2a492a447726968cfdb625a1d1d8dd039
torobucci <kevintoro254@gmail.com>
2023-06-20 17:11:32 +0300

    fix rubocop errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,50 +1,48 @@
 require 'rails_helper'
 
 describe User, type: :request do
-
-  describe "GET /index" do
-    it "returns a successful response" do
-      user1 = User.create!(name: "John", posts_counter: 0)
-      user2 = User.create!(name: "Jane", posts_counter: 0)
+  describe 'GET /index' do
+    it 'returns a successful response' do
+      User.create!(name: 'John', posts_counter: 0)
+      User.create!(name: 'Jane', posts_counter: 0)
 
       get users_url
       expect(response).to have_http_status(200)
     end
-    it "renders the correct template" do
-      user = User.create!(name: "John", posts_counter: 0)
+    it 'renders the correct template' do
+      User.create!(name: 'John', posts_counter: 0)
 
       get users_url
 
-      expect(response).to render_template("users/index")
+      expect(response).to render_template('users/index')
     end
-    it "response body displays correct placeholder text" do
-      user = User.create!(name: "John", posts_counter: 0)
+    it 'response body displays correct placeholder text' do
+      User.create!(name: 'John', posts_counter: 0)
 
       get users_url
 
-      expect(response.body).to include("Listing Users")
+      expect(response.body).to include('Listing Users')
     end
   end
 
-  describe "GET /show" do
+  describe 'GET /show' do
+    let(:valid_attributes) do
+      { name: 'John Doe', posts_counter: 0 }
+    end
 
-    let(:valid_attributes) {
-      { name: "John Doe", posts_counter:0 }
-    }
-
-    it "renders a successful response" do
+    it 'renders a successful response' do
       user = User.create! valid_attributes
       get user_url(user)
       expect(response).to have_http_status(200)
     end
 
-    it "renders the correct template" do
+    it 'renders the correct template' do
       user = User.create! valid_attributes
       get user_url(user)
       expect(response).to render_template('users/show')
     end
 
-    it "response body displays correct placeholder text" do
+    it 'response body displays correct placeholder text' do
       user = User.create! valid_attributes
       get user_url(user)
       expect(response.body).to include('Showing a specific User')
