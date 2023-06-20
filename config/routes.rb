e18f81709af41f4a914733529f99bbb68081d219
torobucci <kevintoro254@gmail.com>
2023-06-20 12:56:48 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
 end
